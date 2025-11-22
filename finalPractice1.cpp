ae335ea68651fe87b49ae08770e3d93db97d80bf
Alexandra Ionita <81383088+alexandrairina@users.noreply.github.com>
2025-11-22 13:55:57 +0200

    november

---
+++ finalPractice1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/*int main() {
+
+	// Chapter 10: Working with char. and strings 
+
+	string street;
+	string city;
+	string state;
+	int zipCode;
+
+	cout << "What is the name of the street? " << endl;
+	getline(cin, street);
+
+	cout << "What city are you living in? " << endl;
+	getline(cin, city);
+
+	cout << "What state are you in?" << endl;
+	getline(cin, state);
+
+	cout << "What is your zip code?" << endl;
+	cin >> zipCode;
+
+	cout << street << endl;
+	cout << city << ", " << state << ", " << zipCode << endl;
+
+
+	return 0;
+} */  // char. and strings
+
+ /*int main() {
+    // 11 - Working with Arrays
+	// Ask the user for 3 names. Store these names in an array and then print the 1st name 
+
+	string names[3];
+
+	cout << "Name 1:" << endl;
+	getline(cin, names[0]);
+
+	cout << "Name 2:" << endl;
+	getline(cin, names[1]);
+
+	cout << "Name 3:" << endl;
+	getline(cin, names[2]);
+
+	cout << "Name 1 = " << names[0];
+
+
+	return 0; 
+} */
+
+/*int main() {
+
+	// 12 - Type Conversion (casting) 
+
+	int x = 10;
+	int y = 3;
+	double z = static_cast<double>(x) / y;
+
+	cout << z; 
+
+
+	return 0;
+} */ 
+
+// Loops 
+
+int main() {
+
+	/*
+	! - NOT operator
+	|| - OR operator 
+	&& - AND operator 
+
+	A job applicant should be a US citizen and either have a Bch. degree or at least two yrs of experience
+
+	*/
+
+	bool usCitizen = true; 
+	bool degree = true;
+	cout << "Years of experience:" << endl;
+	int experience;
+	cin >> experience; 
+
+	bool hired = usCitizen && (degree || experience >= 2);
+
+	cout << boolalpha << hired; 
+
+
+
+	return 0;
+}
+
+
+
